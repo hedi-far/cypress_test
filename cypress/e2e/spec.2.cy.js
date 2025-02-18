57a18ed554f5264ed9c18948861203fbbf3ab24c
hedi-far <hedwig.farthofer@outlook.at>
2025-02-18 10:46:53 +0100

    add secondond test spec

---
+++ cypress/e2e/spec.2.cy.js
@@ -0,0 +1,48 @@
+describe('My First Test', () => {
+  it('Visits the Start Page', () => {
+    cy.visit('https://hfcyptest.netlify.app/')
+  })
+})
+
+
+describe('My First Test', () => {
+  it('finds the content "Welcome"', () => {
+    cy.visit('https://hfcyptest.netlify.app/')
+
+    cy.contains('Welcome')
+  })
+})
+
+describe('My First Test', () => {
+  it('checks the formatting of API_NAME"', () => {
+    cy.visit('https://hfcyptest.netlify.app/')
+
+    cy.get('code').contains('API_NAME');
+  })
+})
+
+
+/*
+
+describe('My First Test', () => {
+  it('clicks the link "About" on Start Page', () => {
+    cy.visit('http://localhost:8080')
+
+    cy.contains('About').click()
+  })
+})
+
+
+describe('My First Test', () => {
+  it('clicking "About" navigates to a new url', () => {
+    cy.visit('http://localhost:8080')
+
+    cy.contains('About').click()
+
+    // Should be on a new URL which
+    // includes '/commands/actions'
+    cy.url().should('include', '/about.html')
+  })
+}) 
+  
+*/
